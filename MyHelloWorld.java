c2976e1b444fb31e96a72ac91dcc1f49c612fcf3
radhika <emailid>
2022-03-06 21:52:18 -0800

    Initial commit

---
+++ MyHelloWorld.java
@@ -0,0 +1,5 @@
+public class Radhika_hellpworld {
+    public static void main(String[] args) {
+        System.out.println("Hi Radhika welcome to Java");
+    }
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hi Radhika, welcome to Hello World");
+        System.out.println("I'm running the  Jar file");
+
+    }
+}
